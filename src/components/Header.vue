a9ee741459b7e3c94677cdbac3a05754d52e47f5
babalolaay0 <92514412+babalolaay0@users.noreply.github.com>
2021-10-15 20:53:25 +0100

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+<header>
+<h3>{{ title }}</h3>
+<Button @btn-click="$emit('btn-click')" :text="closeAddTask ? 'Close' : 'Add Task'" :color="closeAddTask ? 'red' : 'green'" />
+</header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    closeAddTask: Boolean
+  },
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px;
+}
+</style>
